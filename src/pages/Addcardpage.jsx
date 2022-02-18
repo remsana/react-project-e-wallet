bd90f09d5b2bb6c168b14e14416f6dfe9416e00c
Chandana MC <mcchand86@gmail.com>
2022-02-18 14:48:59 +0100

    added Addcard page

---
+++ src/pages/Addcardpage.jsx
@@ -0,0 +1,10 @@
+const Addcardpage = () => {
+    return (
+        <div>
+            <h2>Fill the following details to add a new card:</h2>
+            {/* render a component where one can fill in details */}
+        </div>
+    )
+}
+
+export default Addcardpage;
