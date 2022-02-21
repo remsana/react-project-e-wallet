27c609a60c461e2db4cae696b543fb6e7c0a1f7d
Chandana MC <mcchand86@gmail.com>
2022-02-21 09:37:35 +0100

    Checking for error

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { act } from "react-dom/test-utils";
+// import { act } from "react-dom/test-utils";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
