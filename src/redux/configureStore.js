a0b16e2adba991d45b421760746c1ba4132f9cca
Remya Panicker <mak.rem1545@gmail.com>
2022-03-07 21:59:04 +0100

    deleted addcardSlice

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homepageSlice from "./homepageSlice";
-import addcardpageSlice from "./addcardpageSlice";
 import apiSlice from "./apiSlice";
 
 const store = configureStore ({
     reducer: {
-        homepage: homepageSlice,
-        addcard: addcardpageSlice,
+        homepage: homepageSlice,       
         api: apiSlice,
         
     }
